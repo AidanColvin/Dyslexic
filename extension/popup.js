8a2bb7ee30bb37a6df12ff73d2112c0313f37ccb
Aidan Colvin <Aidan-Colvin@outlook.com>
2026-02-08 12:21:56 -0500

    Implement credential loading and connection testing

--- extension/popup.js
+++ extension/popup.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:717410514efeea94d15cf45a8cedd4cf109ded327493c01490145ab7aacb2b5f
-size 2327
+oid sha256:1531b35e8f150bf1b03555d29f275bc70cf448bb20d5e64fbc98785142213ab9
+size 4926
