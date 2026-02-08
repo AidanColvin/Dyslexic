abaa1bf85356eb7b5c0349cd24880f0e115bd964
Aidan Colvin <Aidan-Colvin@outlook.com>
2026-02-08 12:21:16 -0500

    Add authentication and secure endpoints in app.py
    Refactor app.py to implement authentication and secure endpoints. Update logging and CORS settings.

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:615d1caf7b72930b633aeddb2565dafa7cef532859d846b0b6b47d3855bdc371
-size 3921
+oid sha256:dcb3226a0c61f78ed8efb44da8defb697c01519e5567f7c0fe474dc2a2541100
+size 3851
