901f5492a0912b9711b6e73fcf52359f5179f89f
Aidan Colvin <Aidan-Colvin@outlook.com>
2026-02-08 12:14:44 -0500

    Refactor app.py for improved structure and logging

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7be732e94c6a7877321a19677abd61aae76a7e0508ca654530e55baa7749cec
-size 5631
+oid sha256:615d1caf7b72930b633aeddb2565dafa7cef532859d846b0b6b47d3855bdc371
+size 3921
