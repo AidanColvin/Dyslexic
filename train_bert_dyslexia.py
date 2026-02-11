e9d450c5ddaeedc739f3ad80972ddad564fe576f
Aidan Colvin <Aidan-Colvin@outlook.com>
2026-02-11 02:18:49 +0000

    Refactor parse_holbrook function and fix evaluation strategy naming in training arguments

--- train_bert_dyslexia.py
+++ train_bert_dyslexia.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac62870ee6cbcae469e609a34788681064de60117cc1bcbdbbf0c04b67c8094b
-size 2278
+oid sha256:a3a3985e654a1feb713327713ed327671873b4c84b3496232f7c91a5a1df46af
+size 2151
