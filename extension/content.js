c587504611a7f577f8bed3afb50b26f2e068a3be
Aidan Colvin <Aidan-Colvin@outlook.com>
2026-02-10 20:04:36 -0500

    Delete extension directory

--- extension/content.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b0e0cd741766b060cdfb51f653c05a0e6e5c11c5b13855d0c6043dfb303cff7f
-size 4488
--- extension/popup.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4e1c7fee421c48f272b1ad898d87361f91c895af198c2bc867fa80f4dda8ef8e
-size 2930
